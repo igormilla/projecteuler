e694e69315d3b73a412f44ae8ec44bf22366c197
igor milla <igor.milla@telescope.tv>
2018-08-13 15:51:10 +0200

    feat(0006): proper implementation in Go

---
+++ 0006/sum-square-difference.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main(){
+	const amount = 100
+	var sum = amount * ( amount + 1) / 2
+	var sumOfSquares = (2 * amount + 1) * (amount + 1) * amount / 6
+	fmt.Println(sum * sum - sumOfSquares)
+}
