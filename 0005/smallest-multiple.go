ab55e8f1596f249070fd84150c1cdc5fd7f036ad
igor milla <igor.milla@telescope.tv>
2018-08-16 16:42:29 +0200

    style: unifying indent style

--- 0005/smallest-multiple.go
+++ 0005/smallest-multiple.go
@@ -4,25 +4,25 @@ import "fmt"
 import "math"
 
 func main(){
-	const amount = 20
-	primes := []int{2, 3, 5, 7, 11, 13, 17, 19, 23}
-	a := make([]int, 0)
-	var result = 1
-	var i = 0
-	var check = true
-	var limit = math.Sqrt(amount)
-	for primes[i] <= amount {
-		a = append(a, 1)
-		if check{
-			if float64(primes[i]) <= limit {
-				a[i] = int(math.Floor( math.Log(amount) / math.Log(float64(primes[i]) )))
-			}  else {
-				check = false
-			}
-		}
-		result = result *  int(math.Pow(float64(primes[i]), float64(a[i])))
-		i = i + 1
-	}
+    const amount = 20
+    primes := []int{2, 3, 5, 7, 11, 13, 17, 19, 23}
+    a := make([]int, 0)
+    var result = 1
+    var i = 0
+    var check = true
+    var limit = math.Sqrt(amount)
+    for primes[i] <= amount {
+        a = append(a, 1)
+        if check{
+            if float64(primes[i]) <= limit {
+                a[i] = int(math.Floor( math.Log(amount) / math.Log(float64(primes[i]) )))
+            }  else {
+                check = false
+            }
+        }
+        result = result *  int(math.Pow(float64(primes[i]), float64(a[i])))
+        i = i + 1
+    }
 
-	fmt.Println(result)
-}
+    fmt.Println(result)
+    }
--- 0006/sum-square-difference.go
+++ 0006/sum-square-difference.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main(){
-	const amount = 100
-	var sum = amount * ( amount + 1) / 2
-	var sumOfSquares = (2 * amount + 1) * (amount + 1) * amount / 6
-	fmt.Println(sum * sum - sumOfSquares)
+    const amount = 100
+    var sum = amount * ( amount + 1) / 2
+    var sumOfSquares = (2 * amount + 1) * (amount + 1) * amount / 6
+    fmt.Println(sum * sum - sumOfSquares)
 }
